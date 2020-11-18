eb048381fe4ee4477cd1ca767e2198c10d5ffb9b
Anil Niraula <niraulaanil@gmail.com>
2020-11-17 21:21:37 -0500

    Updt 2020 return file

--- files/PensionDatabase.test.R
+++ files/PensionDatabase.test.R
@@ -4,11 +4,7 @@
 #Clean Global Environment
 rm(list = ls())
 
-#ADD "market_investment_return_mva_basis"
-#to the pullStateData(), pullData(), pullSourceData(), and filterData()
-
 ### Load R Packages ###
-
 #install.packages('devtools')
 #library(devtools)
 #devtools::install_github("ReasonFoundation/reasontheme",force = TRUE)
@@ -31,79 +27,18 @@ library(plotly)
 library(plyr)
 library(dplyr)
 
-#Detailed overview of `pensionviewr` package
-#https://github.com/ReasonFoundation/pensionviewr
-
-#######################
-###Check postgreSQL mapping
-
-masterView <- function(source = NULL, expand = FALSE){
-  con <- RPostgres::dbConnect(
-    RPostgres::Postgres(),
-    dbname = "d629vjn37pbl3l",
-    host = "ec2-3-209-200-73.compute-1.amazonaws.com",
-    port = 5432,
-    user = "reason_readonly",
-    password = "p88088bd28ea68027ee96c65996f7ea3b56db0e27d7c9928c05edc6c23ef2bc27",
-    sslmode = "require")
-  # define the query to retrieve the plan data
-
-    query <- paste("select * from", 
-                   if(isTRUE(expand)){paste("plan_attribute")}
-                      else{paste("master_priority_view")}
-                      )
-    
-    result <- RPostgres::dbSendQuery(con, query)
-    #RPostgres::dbBind(result, list(1))
-    all_data <- RPostgres::dbFetch(result) %>%
-      janitor::clean_names()
-    RPostgres::dbClearResult(result)
-    RPostgres::dbDisconnect(con)
-    
-    all_data <- data.table(all_data)
-    
-    if(is.null(source)){
-      all_data <- data.table(all_data)
-      
-    }else if(!is.null(source) & !isTRUE(expand)){
-    all_data <- all_data[ data_source_name == source]
-    
-    }else if(!is.null(source) & source == "Reason" & isTRUE(expand)){
-      all_data <- all_data[ data_source_id == 3]}
-    
-    else{all_data <- data.table(all_data)
-    }
-    
-}
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 399a30950d86810119074ccb599dda55f9097668
-#View(masterView())
-#View(masterView(source = "Reason"))
-#View(masterView(source = "Public Plans Database"))
-#View(masterView(expand = TRUE))
-#View(masterView(source = "Reason", expand = TRUE))
-<<<<<<< HEAD
-    
-    ###All variables
-    #query <- paste("select * from plan_attribute")
-    #View(all_data[ data_source_id == 3])
-    
-    #write.csv(all_data, file = "/Users/anilniraula/Downloads/attribute.names.csv")
-    
 #masterView() function has 2 parameters:
-  #source = "TEXT" and expand = TRUE/FALSE
-  #source allows to filter for a given datat source (e.g. "Reason", "Public Plans Database", "Census")
-  #expand allow to trigger SQL function that either returns list of mapped variables (FALSE - default) or all available variables (TRUE)
 
-    #library(devtools)
-    #library(roxygen2)
-    #masterView.test <- source_url("https://raw.githubusercontent.com/ReasonFoundation/databaseR/master/files/PensionDatabase.test.R")
+  #Types -- `source` = "TEXT" & `expand` = TRUE/FALSE
+  #`source` allows to filter for a given data source (e.g. "Reason", "Public Plans Database", "Census")
+  #`expand` allows to trigger SQL function that either shos mapped variables (FALSE - default) or all available variables (TRUE)
+
+    library(devtools)
+    library(roxygen2)
+    masterView.test <- source_url("https://raw.githubusercontent.com/ReasonFoundation/databaseR/master/masterView.R")
     
 #View all mapped variables
-    #View(masterView.test$value())
+    View(masterView.test$value())
 
 #View Reason source mapped variables
     #View(masterView.test$value(source = "Reason"))
@@ -116,9 +51,57 @@ masterView <- function(source = NULL, expand = FALSE){
     
 #######################
 #########
-=======
+\
     
 #    write.csv(all_data, file = "/Users/anilniraula/Downloads/attribute.names.csv")
 #######################
-#########
->>>>>>> 399a30950d86810119074ccb599dda55f9097668
+    #######################
+    ###Check postgreSQL mapping
+    
+    masterView <- function(source = NULL, expand = FALSE){
+      con <- RPostgres::dbConnect(
+        RPostgres::Postgres(),
+        dbname = "d629vjn37pbl3l",
+        host = "ec2-3-209-200-73.compute-1.amazonaws.com",
+        port = 5432,
+        user = "reason_readonly",
+        password = "p88088bd28ea68027ee96c65996f7ea3b56db0e27d7c9928c05edc6c23ef2bc27",
+        sslmode = "require")
+      # define the query to retrieve the plan data
+      
+      query <- paste("select * from", 
+                     if(isTRUE(expand)){paste("plan_attribute")}
+                     else{paste("master_priority_view")}
+      )
+      
+      result <- RPostgres::dbSendQuery(con, query)
+      #RPostgres::dbBind(result, list(1))
+      all_data <- RPostgres::dbFetch(result) %>%
+        janitor::clean_names()
+      RPostgres::dbClearResult(result)
+      RPostgres::dbDisconnect(con)
+      
+      all_data <- data.table(all_data)
+      
+      if(is.null(source)){
+        all_data <- data.table(all_data)
+        
+      }else if(!is.null(source) & !isTRUE(expand)){
+        all_data <- all_data[ data_source_name == source]
+        
+      }else if(!is.null(source) & source == "Reason" & isTRUE(expand)){
+        all_data <- all_data[ data_source_id == 3]}
+      
+      else{all_data <- data.table(all_data)
+      }
+      
+    }
+    
+    #View(masterView())
+    #View(masterView(source = "Reason"))
+    #View(masterView(source = "Public Plans Database"))
+    #View(masterView(expand = TRUE))
+    #View(masterView(source = "Reason", expand = TRUE))
+    
+    
+
