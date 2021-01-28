a0dc4c48dee31be4bd2b2e19d4ca0123ba9a7084
Anil Niraula <niraulaanil@gmail.com>
2021-01-27 21:17:09 -0500

    Updated GainLoss code

--- GainLoss.R
+++ GainLoss.R
@@ -37,8 +37,8 @@ library(purrr)
 library(rpart)
 library(vip)
 
-View(pl <- planList())
-x <- pullStateData(2001)
+#View(pl <- planList())
+#x <- pullStateData(2001)
 #view <- masterView("Reason", TRUE)
 #View(view)
 
@@ -46,68 +46,23 @@ x <- pullStateData(2001)
 #Pull, but not use, "actuarial_experience_dollar" (aggregated)
 ##2. If Interest_on_Debt is not available? Calculate?
 ##3. If "amortization_payment_total_amount"is not available? Calculate?
-##4. Do Excel G/L matches Database results?
+##4. Do Excel G/L matches Database results & UAL change?
 
-#Pull state-level data from the database
+####
+#Some columns are not filled (especially demographic (e.g. mortality, new entrants, disability, payroll))
+#Look into the Code aggregating Survival, Mortality, Survival, Disability data for actives, retired, inactives, total
+#Compare Net Change to UAL vs. Actual UAL change for 5-10 plans
+
+#Pull plan-specific data from the database
 pl <- planList()
-#View(pl)
-reason.data <- pullSourceData(pl, "Louisiana State Employees Retirement System", 2001)# (2002-2019)
-
+reason.data <- pullData(pl, "Louisiana State Employees Retirement System")# (2002-2020)
+#reason.data <- pullData(pl, "Louisiana State Employees Retirement System")# (2002-2019)
 #reason.data <- pullData(pl, "New Mexico Public Employees Retirement Association")# (2002-2019)
 #reason.data <- pullData(pl, "Teachers’ Retirement System of Louisiana")# (2002-2019)
 #reason.data <- pullData(pl, "Arizona Public Safety Personnel Retirement System") (2002-2019)
 #reason.data <- pullData(pl, "Georgia Teachers Retirement System")# (2002-2018)
 
-#View(colnames(reason.data))
-#View(reason.data$amortization_payment_total_amount)
-
-#Select existing Gain/Loss columns
-reason.data <- data.table(
-  reason.data  %>%
-    filter(year > 2000) %>%
-    select(year, display_name, state, 
-           investment_experience_dollar, 
-           interest_on_debt_dollar,
-           other_actuarial_experience_dollar,
-           legislative_changes_dollar,
-           changes_to_methods_assumptions_dollar,
-           gain_or_loss_due_to_changes_in_benefits,
-           gain_or_loss_due_to_changes_in_cola_provisions,
-           gain_or_loss_due_to_changes_in_pbi_provisions,
-           #other_interest_dollar,
-           #gain_or_loss_due_to_changes_in_normal_cost_prior_year,
-           #gain_or_loss_due_to_changes_in_other_interest,
-           #age_of_retirement_experience_dollar,
-           #disability_claim_experience_active_dollar,
-           #disability_claim_experience_inactive_dollar,
-           #disability_claim_experience_retired_dollar,
-           #disability_claim_experience_total_dollar,
-           #mortality_rate_experience_active_dollar,
-           #mortality_rate_experience_inactive_dollar,
-           #mortality_rate_experience_retired_dollar,
-           #mortality_rate_experience_total_dollar,
-           #survival_claim_experience_active_dollar,
-           #survival_claim_experience_inactive_dollar,
-           #survival_claim_experience_retired_dollar,
-           #survival_claim_experience_total_dollar",
-           #new_entrant_experience_dollar,
-           salary_experience_dollar,
-           #payroll_experience_dollar,
-           #withdrawal_experience_dollar,
-           #rehire_experiennce_dollar,
-           contribution_deficiency_dollar,
-           #interest_smoothing_dollar,
-       ###Extra non-G/L columns
-           amortization_payment_total_amount,
-           unfunded_actuarially_accrued_liabilities_dollar)
-)
-
-#View(masterView("Public Plans Database"))
-#View(masterView("Public Plans Database", TRUE))
-x <- masterView("Reason")
-x <- x %>% filter(plan_attribute_id >= 10798)
-#10798-10829
-#View(x$master_attribute_name[19:63])
+#### Create a list of all Gain/Loss columns we have ####
 
 gain.loss.names <- c("investment_experience_dollar",
                      "age_of_retirement_experience_dollar",
@@ -133,59 +88,118 @@ gain.loss.names <- c("investment_experience_dollar",
                      "non_investment_actuarial_experience_dollar",
                      "actuarial_experience_dollar",
                      "legislative_changes_dollar",
-                     "changes_to_methods_&_assumptions_dollar",
-                     "interest_on_debt_dollar_dollar",
+                     "changes_to_methods_assumptions_dollar",
+                     "interest_on_debt_dollar",
+                     "other_interest_dollar",
                      "gain_or_loss_due_to_changes_in_benefits",
                      "gain_or_loss_due_to_changes_in_COLA_provisions",
-                     "gain_or_loss_due_to_changes_in_PBI_provisions",
+                     "gain_or_loss_due_to_changes_in_pbi_provisions",
                      "gain_or_loss_due_to_changes_in_normal_cost_prior_year",
                      "gain_or_loss_due_to_changes_in_other_interest",
-                     "contribution_deficiency_dollar")
-#View(colnames(reason.data))
+                     "contribution_deficiency_dollar",
+                     #Non-G/L columns that may be empty
+                     "amortization_payment_total_amount",
+                     "total_amortization_payment_percentage",
+                     "covered_payroll_dollar",
+                     "fiscal_year_of_contribution",
+                     "unfunded_actuarially_accrued_liabilities_dollar")
 
-####
-#Some columns are not filled (especially demographic (e.g. mortality, new entrants, disability, payroll))
-#Look into the Code aggregating Survival, Mortality, Survival, Disability data for actives, retired, inactives, total
-#Compare Net Change to UAL vs. Actual UAL change for 5-10 plans
+## How many columns are in the pulled data?
+col1 <- ncol(reason.data)
+col1
 
-#View(reason.data)
+#### Save names of all the Gain/Loss columns that are currently not in the pulled data
+columns2 <- c("test")
+for (i in (1:length(gain.loss.names))) {
+  if (sum(colnames(reason.data) %in% gain.loss.names[i]) == 0) {
+    columns2 <- rbind(columns2, gain.loss.names[i])
+  }
+}
+
+## Add these columns as empty ones (NA) to the original data
+columns2 <- as.character(columns2 <- columns2[2:length(columns2)])
+cols <- matrix(NA, length(reason.data[, 1]), length(columns2))
+colnames(cols) <- columns2
+reason.data <- cbind(reason.data, cols)
 reason.data <- data.table(reason.data)
 
-#Fill all NAs w/ 0
+## Number of columns after we added missing G/L columns
+col2 <- ncol(reason.data)
+col2
+#How many of the total 38 G/L columns were empty & added
+col2-col1
+
+## Filter for G/L columns only
+reason.data <- reason.data %>%
+  select(year, state, plan_name, gain.loss.names) %>%
+  filter(year > 2000)
+
+#View(masterView("Public Plans Database", TRUE))
+#x <- masterView("Reason")
+#x <- x %>% filter(plan_attribute_id >= 10798)
+#10798-10829
+#View(x$master_attribute_name[19:63])
+
+
+## Fill all NAs w/ 0
+reason.data <- data.table(reason.data)
 reason.data <- reason.data[,lapply(.SD,function(x){ifelse(is.na(x),0,x)})]
 
-#Make sure all numbers are in numeric format
-reason.data[,4:15] <- data.table(reason.data[,4:15]) %>% dplyr::mutate_all(as.numeric)
+## Make sure all column numbers are numeric
+reason.data[,4:(length(reason.data)-1)] <- data.table(reason.data[,4:(length(reason.data)-1)]) %>% dplyr::mutate_all(as.numeric)
 reason.data$year <- as.numeric(reason.data$year)
 
-#Add Net Amortization calculation 
+#### Add Net Amortization ####
 #(Interest on Debt + Amortization Payments)
-reason.data <- reason.data[,net_amo := (interest_on_debt_dollar + amortization_payment_total_amount)]
-View(reason.data)
-ual <- reason.data$unfunded_actuarially_accrued_liabilities_dollar
-#View(reason.data)
+#Code alternative way>> 
+#Amo. contribution % * Future Payroll
+future.payroll <- reason.data[year >= fiscal_year_of_contribution[1]]$covered_payroll_dollar
+future.payroll <- t(cbind(t(future.payroll),reason.data[year == 2020]$covered_payroll_dollar*(1+0.03)))#Use assumed payroll growth rate
+reason.data$payroll2 <- future.payroll
+
+## Calculate net amortization
+reason.data <- reason.data[,net_amo := (interest_on_debt_dollar + 
+                                      if(sum(reason.data$amortization_payment_total_amount)!=0){
+                                        amortization_payment_total_amount}else{
+                                        total_amortization_payment_percentage * payroll2})]
+
+## Save actual UAL for future comparison w/ G/L                                                   
+ual <- data.table(reason.data %>% select(year, state, plan_name, 
+                                         unfunded_actuarially_accrued_liabilities_dollar))
+#Remove columns used for Net Amortization
 reason.data <- reason.data %>% select(!interest_on_debt_dollar & 
                                         !amortization_payment_total_amount & 
-                                        !unfunded_actuarially_accrued_liabilities_dollar)
+                                        !unfunded_actuarially_accrued_liabilities_dollar &
+                                        !actuarial_experience_dollar &
+                                        !total_amortization_payment_percentage &
+                                        !covered_payroll_dollar &
+                                        !fiscal_year_of_contribution &
+                                        !payroll2)
 
+#View(reason.data)
 #View(reason.data %>%
 #       summarise(
 #         across(c(colnames(reason.data[,4:13])),  .fns = list(sum)))
 #)
+##### Calculate Total G/L Change for Each Year ####
+x <- length(reason.data)
 
-#Calculate Total G/L by Year
-for(i in (1:(max(reason.data$year)-min(reason.data$year)))){
-  reason.data$total[i] <- sum(reason.data[i,4:13])
+for(i in (1:reason.data[,.N])){
+  reason.data$total[i] <- sum(reason.data[i,4:x])
 }
 #View(reason.data)
-#View(ual)
-#Net Change to UAL for 2001+ period (in $Billions)
-gl.change <- sum(reason.data[2:19]$total)/1000000000
+## Compare ##
+
+# #Aggregate Gain/Loss change for 2002+ period (in $Billions)
+gl.change <- sum(reason.data[2:20]$total)/1000000000
 gl.change
-#Actual change in UAL for 2001+ period (in $Billions)
-ual.change <- (max(ual)-ual[1])/1000000000
-max(ual)-ual[1]
-#Difference between cumulative G/L (2002-2019) & actual change in UAL (2001-2019)
+
+## Actual change in UAL for 2001+ period (in $Billions)
+ual.change <- (max(ual[year == 2020]$unfunded_actuarially_accrued_liabilities_dollar)
+               -ual[year == 2001]$unfunded_actuarially_accrued_liabilities_dollar)/1000000000
+ual.change
+
+## Difference between cumulative G/L  & actual change in UAL
 round((ual.change - gl.change),4)*1000 # in $Millions
 ############
 ############
